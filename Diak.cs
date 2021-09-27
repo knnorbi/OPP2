23286889a835f69835096841d27a50c751cb6ad6
knnorbi <kluka.norbert@gmail.com>
2021-09-27 14:23:09 +0200

    Initial commit

---
+++ Diak.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP2
+{
+    class Diak
+    {
+        string nev;
+        int magyar;
+        int matek;
+        bool protekcio;
+
+        public Diak(string nev, int magyar, int matek, bool protekcio = false)
+        {
+            this.nev = nev;
+            this.magyar = magyar;
+            this.matek = matek;
+            this.protekcio = protekcio;
+        }
+
+        int Osszpontszam()
+        {
+            return magyar + matek;
+        }
+
+        public bool FelvettekE()
+        {
+            return Osszpontszam() >= 65 || protekcio;
+        }
+
+        public string Nev()
+        {
+            return nev;
+        }
+
+        public void setNev(string nev)
+        {
+            if(nev.Contains(" "))
+                this.nev = nev;
+        }
+
+        public static void DiakokKiir(List<Diak> diakok)
+        {
+            Console.WriteLine("Név\tMagyar\tMatek");
+            foreach (var item in diakok)
+            {
+                Console.WriteLine($"{item.nev}\t{item.magyar}\t{item.matek}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            List<Diak> jelentkezok = new List<Diak>();
+            for (int i = 0; i < 10; i++)
+            {
+                jelentkezok.Add(new Diak(
+                    $"Diak{i + 1}",
+                    r.Next(20, 51),
+                    r.Next(15, 51)));
+            }
+            jelentkezok.Add(new Diak("Petike", 0, 0, true));
+            Diak.DiakokKiir(jelentkezok);
+
+            foreach (var item in jelentkezok)
+            {
+                if (item.FelvettekE())
+                {
+                    Console.WriteLine(item.Nev());
+                }
+            }
+        }
+    }
+}
